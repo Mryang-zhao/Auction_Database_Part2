b781b91ba0ce3ed107d229f2d5c51f6996d4ed6d
Raudel Valdes <raudel.valdes2015@gmail.com>
2019-07-15 20:14:00 -0400

    no longer using the python parser that I created. Now I am using CSV files that I made by exporting from a sqlite db

--- create.sql
+++ create.sql
@@ -1,66 +1,52 @@
-DROP TABLE IF EXISTS AUCTIONS;
-DROP TABLE IF EXISTS SELLERS;
-DROP TABLE IF EXISTS BIDDERS;
-DROP TABLE IF EXISTS BIDS;
+DROP TABLE IF EXISTS USERS;
 DROP TABLE IF EXISTS ITEMS;
+DROP TABLE IF EXISTS BIDS;
 DROP TABLE IF EXISTS CATEGORIES;
+DROP TABLE IF EXISTS CurrentTime;
 
--- Table Schema -> itemID|name|started|ends|sellerID|buy_price|first_bid|number_of_bids
-CREATE TABLE AUCTIONS (
-  itemID INTEGER,
-  name VARCHAR,
-  auct_started DATETIME,
-  auct_ends DATETIME,
-  sellerID VARCHAR,
-  buy_price FLOAT,
-  first_bid FLOAT,
-  current_price FLOAT,
-  number_bids INTEGER,
-  PRIMARY KEY (itemID)
-  FOREIGN KEY (itemID) REFERENCES ITEMS (userID)
-);
-
--- Table Schema -> userID|user_role|rating|user_location|country
-CREATE TABLE SELLERS (
-  userID VARCHAR,
-  rating INTEGER,
-  user_location VARCHAR,
-  country VARCHAR,
+CREATE TABLE USERS (
+  UserID VARCHAR,
+  Location VARCHAR,
+  Country VARCHAR,
+  Rating VARCHAR,
   PRIMARY KEY (userID)
 );
 
--- Table Schema -> userID|user_role|rating|user_location|country
-CREATE TABLE BIDDERS (
-  userID VARCHAR,
-  rating INTEGER,
-  user_location VARCHAR,
-  country VARCHAR,
-  PRIMARY KEY (userID)
-);
-
--- Table Schema -> itemID|sellerID|item_description
 CREATE TABLE ITEMS (
-  itemID VARCHAR,
-  sellerID VARCHAR,
-  item_description INTEGER,
+  ItemID INT,
+  Name VARCHAR,
+  Currently REAL,
+  Buy_Price REAL,
+  First_Bid REAL,
+  Number_of_Bids INT,
+  Started DATETIME,
+  Ends DATETIME,
+  SellerID VARCHAR,
+  Description VARCHAR,
   PRIMARY KEY (itemID)
 );
 
--- Table Schema -> itemID|category
+
 CREATE TABLE CATEGORIES (
-  itemID INTEGER,
-  category VARCHAR,
+  ItemID INTEGER,
+  Category VARCHAR,
   PRIMARY KEY (itemID, category)
 );
 
--- Table Schema -> bidderID|ItemID|amount|current_bid|bid_time
 CREATE TABLE BIDS (
-  bidderID VARCHAR,
-  itemID INTEGER,
-  amount FLOAT,
-  current_bid FLOAT,
-  bid_time DATETIME,
-  PRIMARY KEY (bidderID, itemID, amount),
-  FOREIGN KEY (bidderID) REFERENCES SEllERS (userID),
-  FOREIGN KEY (bidderID) REFERENCES BIDDERS (userID)
+  ItemID INTEGER,
+  BidderID VARCHAR,
+  Amount REAL,
+  Time DATETIME,
+  PRIMARY KEY (BidderID, ItemID, Amount),
+  FOREIGN KEY (BidderID) REFERENCES Users(UserID)
 );
+
+CREATE TABLE CurrentTime (
+  Use_Current_Time DATETIME
+);
+
+INSERT INTO CurrentTime
+VALUES('2001-12- 20 00:00:01');
+
+SELECT * FROM CurrentTime;
